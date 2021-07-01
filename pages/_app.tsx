a4af684bb4dbe9530f54c67015f7c9835582c71e
nazeudon <nazeudon48112@icloud.com>
2021-07-01 09:06:21 +0900

    edited app component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
 import { AppProps } from 'next/app'
+import { useUserChanged } from '../hooks/useUserChanged'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const {} = useUserChanged()
+
   return <Component {...pageProps} />
 }
 
