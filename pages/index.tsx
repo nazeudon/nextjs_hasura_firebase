8bc467adec40000eea92c407e705808353c3901a
nazeudon <nazeudon48112@icloud.com>
2021-07-05 21:17:13 +0900

    :white_check_mark: added ssg into index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,10 @@
+import { GetStaticProps } from 'next'
+import { QueryClient, useQueryClient } from 'react-query'
+import { dehydrate } from 'react-query/hydration'
 import { Auth } from '../components/Auth'
 import { Layout } from '../components/Layout'
+import { fetchNews } from '../hooks/useQueryNews'
+import { News } from '../types/types'
 
 export default function Home() {
   return (
@@ -7,4 +12,16 @@ export default function Home() {
       <Auth />
     </Layout>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const queryClient = new QueryClient()
+  await queryClient.prefetchQuery('news', fetchNews)
+
+  return {
+    props: {
+      // pre-fetchした時にcacheからデータを取得し、jsonに吐き出す
+      dehydratedState: dehydrate(queryClient),
+    },
+  }
 }
