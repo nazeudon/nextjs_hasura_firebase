de623c48d74207ed974dcdb45d08415c7968e2d2
nazeudon <nazeudon48112@icloud.com>
2021-07-05 09:19:20 +0900

    :zap: added reset state in logout

--- hooks/useLogout.ts
+++ hooks/useLogout.ts
@@ -1,15 +1,25 @@
 import Cookie from 'universal-cookie'
+import { useQueryClient } from 'react-query'
+import { useDispatch } from 'react-redux'
+import { resetEditedTask, resetEditedNews } from '../slices/uiSlice'
 import firebase from '../firebaseConfig'
 import { unSubMeta } from './useUserChanged'
 
 const cookie = new Cookie()
 
 export const useLogout = () => {
+  const queryClient = useQueryClient()
+  const dispatch = useDispatch()
+
   const logout = async () => {
     if (unSubMeta) {
       unSubMeta()
     }
+    dispatch(resetEditedTask())
+    dispatch(resetEditedNews())
     await firebase.auth().signOut()
+    queryClient.removeQueries('tasks')
+    queryClient.removeQueries('news')
     cookie.remove('token')
   }
 
